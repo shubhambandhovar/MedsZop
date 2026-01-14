2440bdee8f6f1b3c72b15c7c1bb09f69eb244ad7
Shreya2345shrivastava <shrivastavashreya071@gmail.com>
2026-01-15 00:11:02 +0530

    Fix cors issues

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -21,7 +21,9 @@ app.use(cors({
   origin: [
     process.env.FRONTEND_URL || 'http://localhost:5174',
     'http://localhost:5173',
-    'http://localhost:5175'
+    'http://localhost:5175',
+    'https://medszop-frontend.vercel.app'
+
   ],
   credentials: true
 }));
